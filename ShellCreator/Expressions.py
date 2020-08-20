b65ece348f99bc36a9060cd2345f0fc21b736a3b
Mohammad Ewais <mohammad.a.ewais@gmail.com>
2020-08-20 05:09:23 -0400

    Support for variables in strings

--- ShellCreator/Expressions.py
+++ ShellCreator/Expressions.py
@@ -9,12 +9,13 @@ from .Utils.Operators import operator_and, operator_or
 logger = logging.getLogger('Shell')
 
 variable_name = pyparsing.Combine(pyparsing.Literal('$') + pyparsing.Word(pyparsing.alphas, pyparsing.alphanums + '_'))
+variable_name2 = pyparsing.Combine(pyparsing.Literal('${') + pyparsing.Word(pyparsing.alphas, pyparsing.alphanums + '_') + pyparsing.Literal('}'))
 integer = pyparsing.pyparsing_common.signed_integer
 double = pyparsing.pyparsing_common.real
 true = pyparsing.Keyword('True')
 false = pyparsing.Keyword('False')
-string = pyparsing.QuotedString('\'', escChar='\\') | pyparsing.QuotedString('\"', escChar='\\')
-parser = pyparsing.operatorPrecedence(variable_name | double | integer | string | 
+string = pyparsing.QuotedString('\'', escChar='\\', unquoteResults=False) | pyparsing.QuotedString('\"', escChar='\\', unquoteResults=False)
+parser = pyparsing.operatorPrecedence(variable_name | variable_name2 | double | integer | string | 
                                 true | false, [
                                 ('**', 2, pyparsing.opAssoc.RIGHT),
                                 ('-', 1, pyparsing.opAssoc.RIGHT),
@@ -51,7 +52,6 @@ left_binary_operators = {
 def parseExpression(text):
     # Parse things from the shell, these can be assignments to
     # variables, or conditions of if and while.
-    # TODO: add string parsing
     function = parser.parseString(text)[0]
     if not isinstance(function, str) and not isinstance(function, int) and not isinstance(function, float):
         function = function.asList()
@@ -61,19 +61,52 @@ def parseExpression(text):
 def evaluateExpression(ast, builtin_variables, variables):
     def evaluateString(string):
         if string[0] == '$':
-            if string[1:] in builtin_variables:
-                value = builtin_variables[string[1:]]
-            elif string[1:] in variables:
-                value = variables[string[1:]]
+            if string[1] != '{':
+                if string[1:] in builtin_variables:
+                    value = builtin_variables[string[1:]]
+                elif string[1:] in variables:
+                    value = variables[string[1:]]
+                else:
+                    logger.error('Variable {} does not exist.', string[1:])
+                    raise NameError('Variable does not exist')
             else:
-                logger.error('Variable {} does not exist.', string[1:])
-                raise NameError('Variable does not exist')
+                if string[-1] != '}':
+                    logger.error('Unbalanced brackets {}.', string)
+                    raise NameError('Unbalanced brackets.')
+                if string[2:-1] in builtin_variables:
+                    value = builtin_variables[string[2:-1]]
+                elif string[2:-1] in variables:
+                    value = variables[string[2:-1]]
+                else:
+                    logger.error('Variable {} does not exist.', string[2:-1])
+                    raise NameError('Variable does not exist')
         elif string == 'True':
             value = True
         elif string == 'False':
             value = False
         else:
-            value = string
+            if string[0] != string[-1] or (string[0] != '\'' and string[0] != '\"'):
+                logger.fatal('Impossible, should only handle quoted strings in this case. Recieved {}.', string)
+                exit(7)
+            value = string[1:-1]
+            # Tokenize any variables inside this string
+            variable = variable_name | variable_name2
+            spaces = pyparsing.White()
+            # Printables, minus $
+            non_variable = pyparsing.Word('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
+            grammar = pyparsing.ZeroOrMore(spaces | non_variable | variable)
+            values = grammar.parseString(value)
+            print(values)
+            # Now evaluate the variables
+            newValue = ''
+            for i, value in enumerate(values):
+                if i == 0 and value[0] == '$':
+                    newValue += str(evaluateString(value))
+                elif value[0] == '$' and values[i-1][-1] != '\\':
+                    newValue += str(evaluateString(value))
+                else:
+                    newValue += value
+            value = newValue
         return value
     # Eval can be dangerous, so we do this by hand
     if isinstance(ast, str):
