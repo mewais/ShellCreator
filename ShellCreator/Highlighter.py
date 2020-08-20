354b9713df19420ea3dbf05980350784514b6f67
Mohammad Ewais <mohammad.a.ewais@gmail.com>
2020-08-20 05:22:22 -0400

    Update highlighter with extra support

--- ShellCreator/Highlighter.py
+++ ShellCreator/Highlighter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from pygments.lexer import RegexLexer
+from pygments.lexer import RegexLexer, include
 from pygments.token import *
 
 class ShellLexer(RegexLexer):
@@ -15,15 +15,42 @@ class ShellLexer(RegexLexer):
         cls.commands += '|' + name
         cls.tokens = {
             'root': [
+                include('basic'),
+                include('data'),
+                include('interp'),
+                include('math')
+            ],
+            'interp': [
+                (r'\$\{#?', String.Interpol, 'curly'),
+                (r'\$[a-zA-Z_]\w*', Name.Variable),  # user variable
+                (r'\$(?:\d+|[#$?!_*@-])', Name.Variable),      # builtin
+                (r'\$', Text)
+            ],
+            'basic': [
+                ('\\b(' + cls.commands + ')\\b', Keyword),
                 (r'#.*', Comment.Single),
+                (r'\\[\w\W]', String.Escape)
+            ],
+            'data': [
                 (r'(?s)\$?"(\\.|[^"\\$])*"', String.Double),
+                (r'"', String.Double, 'string'),
                 (r"(?s)\$'(\\\\|\\[0-7]+|\\.|[^'\\])*'", String.Single),
-                (r"(?s)'.*?'", String.Single),
-                (r'\$[a-zA-Z_]\w*', Name.Variable),
+                (r"(?s)'.*?'", String.Single)
+            ],
+            'string': [
+                (r'"', String.Double, '#pop'),
+                (r'(?s)(\\\\|\\[0-7]+|\\.|[^"\\$])+', String.Double),
+                include('interp')
+            ],
+            'curly': [
+                (r'\}', String.Interpol, '#pop'),
+                (r'\w+', Name.Variable),
+                include('root')
+            ],
+            'math': [
                 (r'\d+(\.\d+)\b', Number),
                 (r'\d+\b', Number),
                 (r'\bTrue\b', Number),
                 (r'\bFalse\b', Number),
-                ('\\b(' + cls.commands + ')\\b', Keyword),
             ]
         }
